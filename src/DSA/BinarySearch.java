dccd8fc25881831bc2bcdca69ce67050fb3039ba
lnery <lnery@lamar.edu>
2026-04-15 10:42:14 -0500

    DSA implementations

---
+++ src/DSA/BinarySearch.java
@@ -0,0 +1,36 @@
+package DSA;
+
+import java.util.ArrayList;
+
+public class BinarySearch {
+
+    public int binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int binarySearchR(int[] nums, int target, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return binarySearchR(nums, target, left, mid - 1);
+        } else {
+            return binarySearchR(nums, target, mid + 1, right);
+        }
+    }
+}
